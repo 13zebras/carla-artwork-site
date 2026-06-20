436e429ed18cb19b851ed89a9aa87c2b59368983
Tom Stine <13zebras@users.noreply.github.com>
2026-06-19 21:32:58 -0500

    2x images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,21 +7,23 @@ export function Header() {
   return (
     <header className='fixed z-20 flex justify-center bg-background w-full h-48 sm:h-52 xl:h-38'>
       <div className='relative flex flex-col xl:flex-row items-center sm:items-start xl:items-center justify-center xl:justify-start gap-7 xl:gap-20 max-w-7xl w-full h-full px-4 xxs:px-6 xs:px-10 sm:px-12'>
-        <Link to='/' className='hidden xs:block max-w-160 w-full h-auto'>
+        <Link to='/' className='hidden xs:block max-w-162.5 w-full h-auto'>
           <img
-            src='/logo-h-640.webp'
+            src='/header-logos/logo-h-650x55.webp'
+            srcSet='/header-logos/logo-h-650x55.webp 1x, /header-logos/logo-h-1300x110.webp 2x'
             alt='Carla Stine'
-            width='640'
-            height='55'
-            className='hidden xs:block max-w-160 w-full h-auto'
+            width={650}
+            height={55}
+            className='hidden xs:block w-full h-auto'
           />
         </Link>
-        <Link to='/' className='block xs:hidden max-w-83.5 w-full h-auto'>
+        <Link to='/' className='block xs:hidden max-w-83.75 w-full h-auto'>
           <img
-            src='/logo-stacked-334.webp'
+            src='/header-logos/logo-stacked-335x70.webp'
+            srcSet='/header-logos/logo-stacked-335x70.webp 1x, /header-logos/logo-stacked-670x140.webp 2x'
             alt='Carla Stine'
-            width='334'
-            height='69'
+            width={335}
+            height={70}
             className='w-full h-auto'
           />
         </Link>
