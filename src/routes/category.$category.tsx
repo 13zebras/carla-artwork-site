14582e36620c9dab9131a045a0c29b7bdf12f194
Tom Stine <13zebras@users.noreply.github.com>
2026-08-09 15:27:24 -0500

    added grayscale and animate to category

--- src/routes/category.$category.tsx
+++ src/routes/category.$category.tsx
@@ -1,30 +1,36 @@
 import { createFileRoute, notFound } from '@tanstack/react-router';
 
+import { AnimationLayer } from '@/components/AnimationLayer';
 import { Header } from '@/components/Header';
 import { Portfolio } from '@/components/Portfolio';
 import { getCategoryPage } from '@/lib/functions/artworks.functions';
+import { getAnimationGrayscale } from '@/lib/functions/site-settings.functions';
 
 export const Route = createFileRoute('/category/$category')({
   loader: async ({ params }) => {
-    const categoryPage = await getCategoryPage({
-      data: { categorySlug: params.category },
-    });
+    const [categoryPage, { animationGrayscale }] = await Promise.all([
+      getCategoryPage({
+        data: { categorySlug: params.category },
+      }),
+      getAnimationGrayscale(),
+    ]);
 
     if (!categoryPage) {
       throw notFound();
     }
 
-    return categoryPage;
+    return { ...categoryPage, animationGrayscale };
   },
   component: CategoryComponent,
 });
 
 function CategoryComponent() {
-  const { artworks, title, description } = Route.useLoaderData();
+  const { artworks, title, description, animationGrayscale } = Route.useLoaderData();
 
   return (
     <div className='relative'>
       <Header />
+      <AnimationLayer grayscale={animationGrayscale} />
       <Portfolio artworks={artworks} title={title} description={description} />
     </div>
   );
